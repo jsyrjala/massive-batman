990763c454b5a36e335f22b201482a96dfdcbb55
Juha Syrjälä <juha.syrjala@gmail.com>
2014-08-07 13:24:02 +0300

    Dependency upgrade

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
             ]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies
-                   [[org.clojure/tools.namespace "0.2.4"]
+                   [[org.clojure/tools.namespace "0.2.5"]
                     [org.clojure/java.classpath "0.2.2"]
                     [midje "1.6.3" :exclusions [org.clojure/clojure]]
                     ]
@@ -21,7 +21,7 @@
                  [org.clojure/tools.trace "0.7.8"]
 
                  ;; logging
-                 [org.clojure/tools.logging "0.2.6"
+                 [org.clojure/tools.logging "0.3.0"
                   :exclusions
                   [log4j/log4j
                    commons-logging/commons-logging
@@ -31,27 +31,28 @@
                  [org.slf4j/log4j-over-slf4j "1.7.7"]
 
                  ;; database
-                 [org.clojure/java.jdbc "0.3.3"]
+                 [org.clojure/java.jdbc "0.3.5"]
                  [java-jdbc/dsl "0.1.0"]
                  [honeysql "0.4.3"]
                  [org.postgresql/postgresql "9.3-1100-jdbc41"]
-                 [com.zaxxer/HikariCP "1.3.8"]
+                 ;; com.zaxxer/HikariCP-java6 works on java7
+                 [com.zaxxer/HikariCP "2.0.1"]
                  [ragtime/ragtime.core "0.3.7"]
                  [ragtime/ragtime.sql "0.3.7"]
                  ;; database test
-                 [com.h2database/h2 "1.4.178"]
+                 [com.h2database/h2 "1.4.181"]
 
                  ;; web, rest
-                 [org.clojure/data.json "0.2.4"]
-                 [ring/ring-devel "1.2.2"]
-                 [ring/ring-core "1.2.2"]
+                 [org.clojure/data.json "0.2.5"]
+                 [ring/ring-devel "1.3.0"]
+                 [ring/ring-core "1.3.0"]
                  [ring/ring-json "0.3.1"]
                  [compojure "1.1.8"]
                  ;; 2.1.17/2.1.18 have perf problems, https://github.com/http-kit/http-kit/issues/148
                  ;; 2.1.17 and earlier have https://github.com/http-kit/http-kit/issues/127
                  [http-kit "2.1.18"]
                  [cheshire "5.3.1"]
-                 [ring-cors "0.1.2"]
+                 [ring-cors "0.1.4"]
                  [javax.servlet/servlet-api "2.5"]
 
                  ;; structure
@@ -65,14 +66,14 @@
                  ;; api
                  [org.clojars.runa/clj-schema "0.9.4"]
 
-                 [metosin/compojure-api "0.11.4"]
-                 [metosin/ring-swagger-ui "2.0.16-2"]
+                 [metosin/compojure-api "0.14.0"]
+                 [metosin/ring-swagger-ui "2.0.17"]
 
-                 [prismatic/schema "0.2.2"]
-                 [prismatic/plumbing "0.3.0"]
+                 [prismatic/schema "0.2.6"]
+                 [prismatic/plumbing "0.3.3"]
 
                  ;; util
-                 [clj-time "0.7.0"]
+                 [clj-time "0.8.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [slingshot "0.10.3"]
                  ]
